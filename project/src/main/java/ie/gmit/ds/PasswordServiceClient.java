532992d170698779185ed86b7657ab610a38850b
Conor O'Reilly <conor@users-MacBook-Pro-245.local>
2019-10-31 10:26:43 +0000

    User input added in client

--- project/src/main/java/ie/gmit/ds/PasswordServiceClient.java
+++ project/src/main/java/ie/gmit/ds/PasswordServiceClient.java
@@ -1,30 +1,54 @@
 /** Adapted from https://github.com/john-french/distributed-systems-labs/blob/master/grpc-async-inventory/README.md */
 package ie.gmit.ds;
 
+import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public class PasswordServiceClient {
     /** CLIENT INITIALISATION */
     private static final Logger logger = Logger.getLogger(PasswordServiceClient.class.getName());
+    // Channel connects to the gRPC Server
     private final ManagedChannel channel;
-    private final PasswordServiceGrpc.PasswordServiceStub asyncInventoryService;
-    private final PasswordServiceGrpc.PasswordServiceBlockingStub syncInventoryService;
+    // Async stub
+    private final PasswordServiceGrpc.PasswordServiceStub asyncPasswordService;
+    // blocking (sync) stub
+    private final PasswordServiceGrpc.PasswordServiceBlockingStub syncPasswordService;
+
 
     public PasswordServiceClient(String host, int port) {
         channel = ManagedChannelBuilder
                 .forAddress(host, port)
                 .usePlaintext()
                 .build();
-        syncInventoryService = PasswordServiceGrpc.newBlockingStub(channel);
-        asyncInventoryService = PasswordServiceGrpc.newStub(channel);
+        syncPasswordService = PasswordServiceGrpc.newBlockingStub(channel);
+        asyncPasswordService = PasswordServiceGrpc.newStub(channel);
     }
 
     /** SHUTDOWN METHOD */
     public void shutdown() throws InterruptedException {
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
+
+    /** Scanner takes in user input */
+    Scanner userInput = new Scanner(System.in);
+
+    /** Variables to store user input */
+    private String password;
+    private ByteString hashedPassword;
+    private ByteString salt;
+    private int userId;
+
+    /** Method to take in user ID and Password */
+    public void getUserInput(){
+        System.out.println("Enter User ID: ");
+        userId = userInput.nextInt();
+        System.out.println("Enter Password: ");
+        password = userInput.next();
+    }
+
 }

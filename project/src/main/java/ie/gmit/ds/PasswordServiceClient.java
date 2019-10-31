b99a8cd06a2f8d1f76cf438122ac2553055dc0b1
Conor O'Reilly <conor@users-MacBook-Pro-245.local>
2019-10-31 11:14:05 +0000

    hash & validate methods in client

--- project/src/main/java/ie/gmit/ds/PasswordServiceClient.java
+++ project/src/main/java/ie/gmit/ds/PasswordServiceClient.java
@@ -1,16 +1,22 @@
 /** Adapted from https://github.com/john-french/distributed-systems-labs/blob/master/grpc-async-inventory/README.md */
 package ie.gmit.ds;
 
+import com.google.protobuf.BoolValue;
 import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
 
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class PasswordServiceClient {
-    /** CLIENT INITIALISATION */
+    /**
+     * CLIENT INITIALISATION
+     */
     private static final Logger logger = Logger.getLogger(PasswordServiceClient.class.getName());
     // Channel connects to the gRPC Server
     private final ManagedChannel channel;
@@ -29,35 +35,88 @@ public class PasswordServiceClient {
         asyncPasswordService = PasswordServiceGrpc.newStub(channel);
     }
 
-    /** SHUTDOWN METHOD */
+    /**
+     * SHUTDOWN METHOD
+     */
     public void shutdown() throws InterruptedException {
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
 
-    /** Scanner takes in user input */
+    /**
+     * Scanner takes in user input
+     */
     Scanner userInput = new Scanner(System.in);
 
-    /** Variables to store user input */
+    /**
+     * Variables to store user input
+     */
     private String password;
     private ByteString hashedPassword;
     private ByteString salt;
     private int userId;
 
-    /** Method to take in user ID and Password */
-    public void getUserInput(){
+    /**
+     * Method to take in user ID and Password
+     */
+    public void getUserInput() {
         System.out.println("Enter User ID: ");
         userId = userInput.nextInt();
         System.out.println("Enter Password: ");
         password = userInput.next();
     }
 
-    /** METHOD FOR HASH REQUEST */
-    public void hashRequest(){
+    /**
+     * METHOD FOR HASH REQUEST
+     * Returns hash request synchronously by calling the blocking stub (syncPasswordService)
+     */
+    public void hashRequest() {
+        getUserInput();
+
+        HashRequest hashRequest = HashRequest.newBuilder().setUserId(userId).setPassword(password).build();
+
+        HashResponse response;
+
+        try {
+            logger.info("Sending Hash Request ");
+            response = syncPasswordService.hash(hashRequest);
+            hashedPassword = response.getHashedPassword();
+            salt = response.getSalt();
+        }catch (StatusRuntimeException ex){
+            logger.log(Level.WARNING, "RPC failed: {0}", ex.getStatus());
+            return;
+        }
 
     }
-
-    /** METHOD FOR VALIDATION REQUEST */
+    /** METHOD FOR VALIDATION REQUEST
+     * Asynchronous call. Stream observer is passed in handle response from the server */
     public void validationRequest(){
+        StreamObserver<BoolValue> responseObserver = new StreamObserver<BoolValue>() {
+
+            @Override
+            public void onNext(BoolValue value) {
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+            System.out.println("An error has occurred!");
+            }
+
+            @Override
+            public void onCompleted() {
+            System.exit(0);
+            }
+        };
+
+        try {
+            logger.info("Validating Request ");
+            asyncPasswordService.validate(ValidateRequest.newBuilder().build(), responseObserver);
+           // logger.info("Returned from requesting all items ");
+        } catch (
+                StatusRuntimeException ex) {
+            logger.log(Level.WARNING, "RPC failed: {0}", ex.getStatus());
+            return;
+        }
 
     }
 
